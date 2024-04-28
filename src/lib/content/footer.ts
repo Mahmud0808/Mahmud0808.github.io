9c34e32511dfe61b81157b7a75faa6cca7db0d24
Mahmud0808 <crazymahmud08@gmail.com>
2024-04-28 09:18:21 +0000

    Deploying to public from @ Mahmud0808/Mahmud0808.github.io@de2acc177cede26f8a577f0685f0adfc8ecf7517 🚀

--- src/lib/content/footer.ts
+++
@@ -1,6 +0,0 @@
-import { FooterSectionType } from '@/lib/types/sections';
-
-export const footerSection: FooterSectionType = {
-    title: 'Made with ♡ by Mahmudul Hasan',
-    link: 'https://github.com/Mahmud0808',
-};
