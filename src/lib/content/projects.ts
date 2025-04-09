dbdfeb512de09d8823ce1b83ac993bfc8b7969d2
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2025-04-09 21:24:24 +0600

    Update project list

--- src/lib/content/projects.ts
+++ src/lib/content/projects.ts
@@ -8,6 +8,22 @@ export const projectsSection: ProjectsSectionType = {
   projects: [
     {
       id: getId(),
+      name: 'Ryda',
+      url: 'https://github.com/Mahmud0808/Ryda',
+      repo: 'https://github.com/Mahmud0808/Ryda',
+      img: 'https://i.postimg.cc/PqwjjvvL/ryda-banner.jpg',
+      year: 2025,
+      projectSkills: [
+        { name: 'React Native', icon: 'vscode-icons:file-type-reactjs' },
+        { name: 'Expo', icon: 'vscode-icons:file-type-expo' },
+        { name: 'Maps API', icon: 'logos:google-maps' },
+        { name: 'Clerk', icon: 'simple-icons:clerk' },
+        { name: 'NativeWind', icon: 'vscode-icons:file-type-tailwind' },
+        { name: 'Stripe', icon: 'vscode-icons:file-type-silverstripe' },
+      ],
+    },
+    {
+      id: getId(),
       name: 'Chicken Road Game',
       url: 'https://drive.google.com/drive/folders/1E3myxok1MQjIeYxiijvjtTHkg4dvzc7-?usp=sharing',
       repo: 'https://github.com/Mahmud0808/ChickenRoad',
@@ -15,6 +31,9 @@ export const projectsSection: ProjectsSectionType = {
       year: 2025,
       projectSkills: [
         { name: 'Kotlin', icon: 'vscode-icons:file-type-kotlin' },
+        { name: 'Firebase', icon: 'vscode-icons:file-type-firebase' },
+        { name: 'OneSignal', icon: 'logos:onesignal' },
+        { name: 'Facebook SDK', icon: 'logos:facebook' },
       ],
     },
     {
