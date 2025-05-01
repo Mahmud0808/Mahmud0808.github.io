232b3dffdb21a9f7b920e9b5063870437bd94d63
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2025-05-02 01:15:27 +0600

    Replace featured project image with lower quality version

--- src/lib/content/featured-projects.ts
+++ src/lib/content/featured-projects.ts
@@ -11,7 +11,7 @@ const featuredProjectsSection: FeaturedProjectsSectionType = {
       description: 'Open-source Android theming application.',
       tasks: 'Iconify lets you customize your Android 12+ device easily. Change icons, colors, shapes, and even the notification panel for a personalized look that suits your style.',
       url: 'https://github.com/Mahmud0808/Iconify',
-      img: 'https://i.postimg.cc/VzTtxw2M/iconify.png',
+      img: 'https://i.postimg.cc/26L2PSLR/iconify.png',
       projectSkills: [
         { name: 'Kotlin', icon: 'vscode-icons:file-type-kotlin' },
         { name: 'Java', icon: 'vscode-icons:file-type-java' },
@@ -26,7 +26,7 @@ const featuredProjectsSection: FeaturedProjectsSectionType = {
       description: 'Customize Material You colors of your device.',
       tasks: 'An application for customizing Material You colors on devices with Android 12+. It lets you tweak accent colors, background saturation, and more for a personalized look.',
       url: 'https://github.com/Mahmud0808/ColorBlendr',
-      img: 'https://i.postimg.cc/ZmjNmrk4/colorblendr.png',
+      img: 'https://i.postimg.cc/9fnL87MY/colorblendr.png',
       projectSkills: [
         { name: 'Kotlin', icon: 'vscode-icons:file-type-kotlin' },
         { name: 'AIDL', icon: 'logos:android-icon' },
