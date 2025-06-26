800c20a9359e55b13f88d1e88db9c1ecb89931a9
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2025-06-26 16:10:55 +0600

    Make accessible link text nullable

--- src/components/socials/SocialLink.tsx
+++ src/components/socials/SocialLink.tsx
@@ -2,14 +2,14 @@ type Props = {
   children: React.ReactNode;
   className?: string;
   href: string;
-  name: string;
+  name?: string;
 };
 
 const SocialLink = ({ href, name, children, className = '' }: Props) => {
   return (
     <a
       href={href}
-      aria-label={name}
+      aria-label={name || 'Social Link'}
       target="_blank"
       className={`block text-sm p-2.5 hover:text-accent hover:-translate-y-1 focus:outline-none focus:text-accent focus:-translate-y-1 duration-200 ${className}`}
     >
